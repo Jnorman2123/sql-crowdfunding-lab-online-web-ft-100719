0280576c5646ce4683c46d398465cc21b1457635
Jonathan Norman <jnorman2123@gmail.com>
2019-11-05 03:16:32 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount) AS total_pledges FROM users
-  JOIN pledges ON users users.id = pledges.user_id
+  JOIN pledges ON users.id = pledges.user_id
   GROUP BY pledges.user_id
   ORDER BY users.name;"
 end
